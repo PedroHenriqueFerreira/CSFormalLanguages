cb3bf8c0cf9bdb190933135a9edd4c9007878279
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2025-01-27 22:04:44 -0300

    remove cache

--- pda.py
+++ pda.py
@@ -8,37 +8,37 @@ class PDA:
         self._pilha = ['#']
         self.log = False
         
-    def makeLog(self):
-        self.log = True
+def makeLog(self):
+    self.log = True
+    
+def run(self, w: str):
+    c = w[0] if w else None
+    pop = self._pilha[-1]
+    
+    transitions: set[Transition] = set()
+    transitions.update(self.q.transitions(None, None))
+    transitions.update(self.q.transitions(c, None))
+    transitions.update(self.q.transitions(None, pop))
+    transitions.update(self.q.transitions(c, pop))
+    
+    if self.log:
+        print(f'{self.q.name}: {w}')
+        print("".join(self._pilha))
+        print('-' * 40)
+    
+    for transition in transitions:
+        pda = PDA(transition.state)
         
-    def run(self, w: str):
-        c = w[0] if w else None
-        pop = self._pilha[-1]
+        pda._pilha = self._pilha.copy()
+        pda.log = self.log
         
-        transitions: set[Transition] = set()
-        transitions.update(self.q.transitions(None, None))
-        transitions.update(self.q.transitions(c, None))
-        transitions.update(self.q.transitions(None, pop))
-        transitions.update(self.q.transitions(c, pop))
-        
-        if self.log:
-            print(f'{self.q.name}: {w}')
-            print("".join(self._pilha))
-            print('-' * 40)
-        
-        for transition in transitions:
-            pda = PDA(transition.state)
+        if transition.edge.pop:
+            pda._pilha.pop()
             
-            pda._pilha = self._pilha.copy()
-            pda.log = self.log
-            
-            if transition.edge.pop:
-                pda._pilha.pop()
-                
-            if transition.edge.push:
-                pda._pilha.append(transition.edge.push)
+        if transition.edge.push:
+            pda._pilha.append(transition.edge.push)
 
-            if pda.run(w[1:] if transition.edge.c else w):
-                return True
-        
-        return c is None and self.q.is_final
+        if pda.run(w[1:] if transition.edge.c else w):
+            return True
+    
+    return c is None and self.q.is_final
